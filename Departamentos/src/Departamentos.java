6dc5dc7caf534d910c92317847e380f2bf8e9beb
rb <rb@rb-VirtualBox>
2019-09-23 21:09:50 +0200

    encapsular cada parte de el ejercicio

--- Departamentos/src/Departamentos.java
+++ Departamentos/src/Departamentos.java
@@ -1,10 +1,12 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
@@ -13,8 +15,11 @@ import java.util.Properties;
 import java.util.Scanner;
 
 public class Departamentos {
-
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		Departamentos departamentos = new Departamentos();
+		departamentos.createsBinWithPropsyDatInHomeDir();
+		
+		
 		//1.
 		System.out.println(System.getProperty("user.home")+"/bin/configuracion.props");
 		Scanner sc = new Scanner(System.in);
@@ -22,13 +27,22 @@ public class Departamentos {
 		ArrayList<Departamento> departamentos = new ArrayList<>();	
 		Departamentos departamento =new Departamentos();
 		boolean eliminado = false;
+		String binAbsoluteDirOfConfiguracionProps = System.getProperty("user.home")+"/bin/configuracion.props";
+		
 		try {
-		Properties configuracionProps = new Properties();
-		configuracionProps.load(new FileInputStream(System.getProperty("user.home")+"/bin/configuracion.props"));
-		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(configuracionProps.getProperty(System.getProperty("user.home")+"/bin/nombrearchivo")));
+		
+			Properties configuracionProps = new Properties();
+			configuracionProps.load(new FileInputStream(binAbsoluteDirOfConfiguracionProps));
+			String nombrearchivo = configuracionProps.getProperty("nombrearchivo");
+			String binAbsoluteDirOfDepartamentoDat =  
+			System.getProperty("user.home")+"/bin/"+nombrearchivo;
+			
+			System.out.println(nombrearchivo);
+		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(nombrearchivo));
 		Departamento d1 = new Departamento(2,"a","a");
 		oos.writeObject(d1);
-		ObjectInputStream ois = new ObjectInputStream(new FileInputStream(configuracionProps.getProperty(System.getProperty("user.home")+"/bin/nombrearchivo")));
+		ObjectInputStream ois = new ObjectInputStream(new FileInputStream(
+		configuracionProps.getProperty(binAbsoluteDirOfDepartamentoDat)));
 		departamentos.add((Departamento)ois.readObject());
 		//2
 		for(int i = 0;departamentos.size()>i;i++)
@@ -38,7 +52,7 @@ public class Departamentos {
 			System.out.println(departamentos.get(i).getlocalidad());
 		}
 		System.out.println("poner numero de departamento que desea eliminar");
-		
+		//3
 		for(int i = 0;departamentos.size()>i;i++)
 		{
 			if(sc.nextInt()==departamentos.get(i).getnumeroDepartamento())
@@ -52,6 +66,7 @@ public class Departamentos {
 				
 			}
 		}
+		//4
 		for(int i = 0;departamentos.size()>i;i++)
 		{
 			if(sc.nextInt()==departamentos.get(i).getnumeroDepartamento())
@@ -67,5 +82,29 @@ public class Departamentos {
 		oos.close();
 		sc.close();
 		}catch(Exception e ){}
+		
 
-}}
+}
+	void createsBinWithPropsyDatInHomeDir() throws FileNotFoundException, IOException{
+		File bin = new File("bin");
+		File configuracionProps = new File("configuracion.Props");
+		File departamentoDat = new File("departamento.dat");
+		String s = null;
+		Properties properties = new Properties();
+		bin.mkdir();
+		properties.setProperty("nombrearchivo", "departamento.dat");
+		properties.store(new FileOutputStream(configuracionProps),"conf");
+		
+		Process chmodoequalsrw = Runtime.getRuntime().exec
+				("chmod o=rw bin");
+			Process mvFiles = Runtime.getRuntime().exec
+				("mv configuracion.props departamentos.dat  bin");
+			Process mvDir = Runtime.getRuntime().exec
+				("mv bin/ /home/rb"); 
+	        
+		
+		
+		
+	}	
+}
+
