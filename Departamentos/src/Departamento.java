b80a26306e2ae42d552f0e9bde1b01cab690550a
rb <rb@rb-VirtualBox>
2019-09-23 17:27:45 +0200

    q

--- Departamentos/src/Departamento.java
+++ Departamentos/src/Departamento.java
@@ -4,10 +4,17 @@ public class Departamento implements Serializable{
 int numeroDepartamento ;
 String nombre;
 String localidad;
-Departamento(){
-	numeroDepartamento=4;nombre = "roberto";localidad="localidad";
+Departamento(int numeroDepartamento ,
+String nombre,
+String localidad){
+	this.numeroDepartamento=numeroDepartamento;
+	this.nombre = nombre;
+	this.localidad=localidad;
 }
 int getnumeroDepartamento(){return numeroDepartamento;}
 String getnombre (){return nombre;}
 String getlocalidad(){return localidad;}
+void setnumeroDepartamento(int numeroDepartamento){ this.numeroDepartamento=numeroDepartamento;}
+void setnombre (String nombre){this.nombre=nombre;}
+void setlocalidad(String localidad){this.localidad=localidad;}
 }
--- Departamentos/src/Departamentos.java
+++ Departamentos/src/Departamentos.java
@@ -4,39 +4,68 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
 import java.util.Properties;
+import java.util.Scanner;
 
 public class Departamentos {
 
 	public static void main(String[] args) {
-		
-		File bin = new File( System.getProperty("user.home")+"/bin/departamento.dat");
+		//1.
+		System.out.println(System.getProperty("user.home")+"/bin/configuracion.props");
+		Scanner sc = new Scanner(System.in);
+		System.out.println(System.getProperty("user.home"));
+		ArrayList<Departamento> departamentos = new ArrayList<>();	
 		Departamentos departamento =new Departamentos();
+		boolean eliminado = false;
 		try {
-			bin.createNewFile();
-		
 		Properties configuracionProps = new Properties();
+		configuracionProps.load(new FileInputStream(System.getProperty("user.home")+"/bin/configuracion.props"));
+		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(configuracionProps.getProperty(System.getProperty("user.home")+"/bin/nombrearchivo")));
+		Departamento d1 = new Departamento(2,"a","a");
+		oos.writeObject(d1);
+		ObjectInputStream ois = new ObjectInputStream(new FileInputStream(configuracionProps.getProperty(System.getProperty("user.home")+"/bin/nombrearchivo")));
+		departamentos.add((Departamento)ois.readObject());
+		//2
+		for(int i = 0;departamentos.size()>i;i++)
+		{
+			System.out.println(departamentos.get(i).getnumeroDepartamento());
+			System.out.println(departamentos.get(i).getnombre());
+			System.out.println(departamentos.get(i).getlocalidad());
+		}
+		System.out.println("poner numero de departamento que desea eliminar");
 		
-		String nombreArchivo = null;
-		try {
-
-		  configuracionProps.load(new FileInputStream(System.getProperty("user.home")
-				  +"/bin"+"configuracion.props"));
-		  nombreArchivo = configuracionProps.getProperty("nombrearchivo");
-		  ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream
-				  (new File(System.getProperty("user.home")+"/bin"+nombreArchivo)));
-		  oos.writeObject(departamento);
-		
-		  oos.close();
-		  
-		  ObjectInputStream ois = new ObjectInputStream(new FileInputStream
-				  (new File(System.getProperty("user.home")+"/bin/"+nombreArchivo)));
-		 Departamentos obj = (Departamentos) ois.readObject();
-		 System.out.println(obj.);
-		  ois.close();
+		for(int i = 0;departamentos.size()>i;i++)
+		{
+			if(sc.nextInt()==departamentos.get(i).getnumeroDepartamento())
+			{
+				System.out.println("numero de departamento");
+				departamentos.get(i).setnumeroDepartamento(sc.nextInt());
+				System.out.println("nombre");
+				departamentos.get(i).setnombre(sc.next());
+				System.out.println("localidad");
+				departamentos.get(i).setlocalidad(sc.next());
+				
+			}
+		}
+		for(int i = 0;departamentos.size()>i;i++)
+		{
+			if(sc.nextInt()==departamentos.get(i).getnumeroDepartamento())
+			{
+			departamentos.remove(i);
+			System.out.println("este departamento fue eliminado correctamente");
+			eliminado = true;
+			break;
+			}else {eliminado = false;}
+		}
+		if(eliminado == false)
+		System.out.println("este numero de departamento no existe");
+		oos.close();
+		sc.close();
 		}catch(Exception e ){}
 
 }}
