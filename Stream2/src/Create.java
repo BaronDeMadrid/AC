1021ffa97111a57996a07bf6b5bc19fb31a11167
rb <rb@rb-VirtualBox>
2019-10-04 20:57:16 +0200

    com

--- Stream2/src/Create.java
+++ Stream2/src/Create.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.util.Base64;
 
 public class Create
 {
@@ -12,13 +13,8 @@ public class Create
 		Departamento dep = new Departamento();
 		try {
 		File file = new File("departamento.dat");
-		if(file.exists()== true)
-		{
-			create = new Create (file);
-		}
-		else{file.createNewFile();
-		create = new Create (file);
-		}
+		if(file.exists()!= true)
+		file.createNewFile();
 		create.write(dep,file);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
@@ -30,11 +26,32 @@ public class Create
 		raf.write(dep.getNum());
 		raf.write(dep.getName());
 	}
-	private void modify(String s,File file) throws FileNotFoundException{
+	private void checksIfExists(String contains,File file,String replace) throws IOException{
+		byte [] modify = new byte[20];
+		String compare = null;
 		RandomAccessFile raf = new RandomAccessFile(file, "rw");
-		while 
-	}
+		while (raf.getFilePointer()<file.length())
+		raf.seek(raf.getFilePointer()+4);
+			for(int i = 0 ;i<10;i++)
+			{
+				modify[i] +=raf.readByte();
+			}
+			compare = Base64.getEncoder().encodeToString(modify);
+		if(contains.contentEquals(compare))
+		{
+			modify(raf.getFilePointer()-20,replace,file);
+		}else{System.out.println("doesn't exist");}
 	
+	
+	}
+	void modify (long pointer,String replace,File file) throws IOException{
+		RandomAccessFile raf = new RandomAccessFile(file,"w");
+		byte[] bytes = replace.getBytes();
+		for(int i = 0;i<bytes.length;i++) 
+		{
+			raf.write(bytes[i]);
+		}
+	}
 	
 }
 
